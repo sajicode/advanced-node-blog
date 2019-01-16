6547582cfcec6cff6106ff279cb209d700d8ea10
sajicode <sajioloye@gmail.com>
2019-01-16 02:48:38 +0100

    modify dev database path

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
 	googleClientID: '403862610013-m4t8l3drle9j7fdrb2f72iog44vjhadt.apps.googleusercontent.com',
 	googleClientSecret: '5m18r7r-JCJQBa0nGoph0Hdb',
-	mongoURI: 'mongodb://saji:25tolife@ds145184.mlab.com:45184/blog_dev',
+	mongoURI: 'mongodb://localhost:27017/blog-dev',
 	cookieKey: 'sepeteriaintsamiganja'
 };
