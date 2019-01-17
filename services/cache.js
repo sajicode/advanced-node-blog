cac47e88071e0252e33349cc2eede9fba837995b
sajicode <sajioloye@gmail.com>
2019-01-17 12:55:09 +0100

    setup distinct caching keys

--- services/cache.js
+++ services/cache.js
@@ -5,5 +5,14 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = function() {
 	console.log('I am about to run a query');
 
+	console.log(this.getQuery());
+	console.log(this.mongooseCollection.name);
+	// create a copy of this.getQuery and add collection property to it
+	const key = Object.assign({}, this.getQuery(), {
+		collection: this.mongooseCollection.name
+	});
+
+	console.log(key);
+
 	return exec.apply(this, arguments);
 };
