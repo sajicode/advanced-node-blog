1e6c3775d3a154b12038eaa8432aff5e0f7c3948
sajicode <sajioloye@gmail.com>
2019-01-18 17:59:24 +0100

    setup jest to access mongoose and userfactory boilerplate

--- tests/setup.js
+++ tests/setup.js
@@ -1,1 +1,10 @@
 // jest.setTimeout(30000);
+
+// require user model so mongoose knows user collection exists
+require('../models/User');
+
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
