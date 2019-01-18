5525645d4427172a78acf44313c7e79490f464cf
sajicode <sajioloye@gmail.com>
2019-01-18 03:55:17 +0100

    launch chromium instance in headless mode

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,20 @@
+const puppeteer = require('puppeteer');
+
 test('adds two numbers', () => {
 	const sum = 1 + 2;
 
 	expect(sum).toEqual(3);
 });
+
+// all operations with puppeteer are async
+test(
+	'we can launch a browser',
+	async () => {
+		const browser = await puppeteer.launch({
+			headless: true,
+			args: [ '--no-sandbox', '--disable-setuid-sandbox' ]
+		});
+		const page = await browser.newPage();
+	},
+	30000
+);
