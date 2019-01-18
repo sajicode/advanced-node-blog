3b20f05d4a76a6051752ad32854bb5612625a84f
sajicode <sajioloye@gmail.com>
2019-01-18 13:47:07 +0100

    create new dev keys file; setup google oauth testing

--- tests/header.test.js
+++ tests/header.test.js
@@ -15,8 +15,16 @@ afterEach(async () => {
 	await browser.close();
 });
 // all operations with puppeteer are async
-test('we can launch a browser', async () => {
+test('the header has the correct test', async () => {
 	const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
 
 	expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+	await page.click('.right a');
+
+	const url = await page.url();
+
+	expect(url).toMatch(/accounts\.google\.com/);
 });
