10bac6c4b7bd423daae65c7230ff9bd49866cde3
sajicode <sajioloye@gmail.com>
2019-01-19 10:07:23 +0100

    factor in login method into page helper

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -1,22 +1,37 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('../factories/sessionFactory');
+const userFactory = require('../factories/userFactory');
 
 class CustomPage {
 	static async build() {
 		const browser = await puppeteer.launch({
-			headless: false
+			headless: true,
+			args: [ '--no-sandbox' ]
 		});
 		const page = await browser.newPage();
 		const customPage = new CustomPage(page);
 
 		return new Proxy(customPage, {
 			get: function(target, property) {
-				return customPage[property] || page[property] || browser[property];
+				return customPage[property] || browser[property] || page[property];
 			}
 		});
 	}
 
 	constructor(page) {
 		this.page = page;
+	}
+
+	async login() {
+		const user = await userFactory();
+
+		const { session, sig } = sessionFactory(user);
+
+		// set cookies on chromium header
+		await this.page.setCookie({ name: 'session', value: session });
+		await this.page.setCookie({ name: 'session.sig', value: sig });
+		await this.page.goto('localhost:3000');
+		await this.page.waitFor('a[href="/auth/logout"]');
 	}
 }
 
