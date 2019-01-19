3ae8ecf1932815978907bd54259e383c242e9c25
sajicode <sajioloye@gmail.com>
2019-01-19 09:43:04 +0100

    integrate custompage into headertest

--- tests/setup.js
+++ tests/setup.js
@@ -1,10 +1,9 @@
-// jest.setTimeout(30000);
-
-// require user model so mongoose knows user collection exists
-require('../models/User');
+jest.setTimeout(30000);
 
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
+// require user model so mongoose knows user collection exists
+require('../models/User');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useMongoClient: true });
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
