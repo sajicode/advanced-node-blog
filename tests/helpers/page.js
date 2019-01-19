e3a5a504c8464756c5bcac7a0ecd46cc70180e6d
sajicode <sajioloye@gmail.com>
2019-01-19 10:21:42 +0100

    complete page.js helper

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -8,6 +8,7 @@ class CustomPage {
 			headless: true,
 			args: [ '--no-sandbox' ]
 		});
+
 		const page = await browser.newPage();
 		const customPage = new CustomPage(page);
 
@@ -24,14 +25,53 @@ class CustomPage {
 
 	async login() {
 		const user = await userFactory();
-
 		const { session, sig } = sessionFactory(user);
 
-		// set cookies on chromium header
 		await this.page.setCookie({ name: 'session', value: session });
 		await this.page.setCookie({ name: 'session.sig', value: sig });
-		await this.page.goto('localhost:3000');
+		await this.page.goto('http://localhost:3000/blogs');
 		await this.page.waitFor('a[href="/auth/logout"]');
+	}
+
+	async getContentsOf(selector) {
+		return this.page.$eval(selector, (el) => el.innerHTML);
+	}
+
+	get(path) {
+		return this.page.evaluate((_path) => {
+			return fetch(_path, {
+				method: 'GET',
+				credentials: 'same-origin',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}).then((res) => res.json());
+		}, path);
+	}
+
+	post(path, data) {
+		return this.page.evaluate(
+			(_path, _data) => {
+				return fetch(_path, {
+					method: 'POST',
+					credentials: 'same-origin',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(_data)
+				}).then((res) => res.json());
+			},
+			path,
+			data
+		);
+	}
+
+	execRequests(actions) {
+		return Promise.all(
+			actions.map(({ method, path, data }) => {
+				return this[method](path, data);
+			})
+		);
 	}
 }
 
