6c9856b1db1995a9dece554eb84e41d8d59d3be8
sajicode <sajioloye@gmail.com>
2019-01-20 09:13:08 +0100

    get uploaded file and create img upload route

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,10 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+	state = {
+		file: null
+	};
+
 	renderFields() {
 		const { formValues } = this.props;
 
@@ -41,7 +45,11 @@ class BlogFormReview extends Component {
 
 		const { submitBlog, history, formValues } = this.props;
 
-		submitBlog(formValues, history);
+		submitBlog(formValues, this.state.file, history);
+	}
+
+	onFileChange(event) {
+		this.setState({ file: event.target.files[0] });
 	}
 
 	render() {
@@ -51,7 +59,7 @@ class BlogFormReview extends Component {
 				{this.renderFields()}
 
 				<h5>Add an Image</h5>
-				<input type="file" accept="image/*" />
+				<input onChange={this.onFileChange.bind(this)} type="file" accept="image/*" />
 
 				{this.renderButtons()}
 			</form>
